25c7ebb86f3394de67428e7c2909111275329cdc
withnahidul <nahidulislam6800@gmail.com>
2022-05-19 00:54:42 +0600

    todolist

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import Form from './Components/Form';
+import TodoList from './Components/TodoList';
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
         <h1>Nis Todo List</h1>
       </header>
       <Form></Form>
+      <TodoList></TodoList>
     </div>
   );
 }
